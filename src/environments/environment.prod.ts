97d86e98759b9a4170d0f8a53d63d02cf9f491ad
walid <walid.dev@outlook.fr>
2023-03-25 14:30:01 +0100

    env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCGp1kwQP-P3S8_0RzjRRZd7zvhaG9tpxw",
+    authDomain: "pellet-74b44.firebaseapp.com",
+    databaseURL: "https://pellet-74b44-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "pellet-74b44",
+    storageBucket: "pellet-74b44.appspot.com",
+    messagingSenderId: "83534619956",
+    appId: "1:83534619956:web:2c63c543b998824dfb3161",
+    measurementId: "G-WSNLY378Q7"
+  }
 };
