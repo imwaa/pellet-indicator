93d018c1cdbb7e4bfe77810c72ac7a1316635c46
walid <walid.dev@outlook.fr>
2023-03-25 11:34:23 +0100

    First commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,19 @@ export const environment = {
   production: false
 };
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCGp1kwQP-P3S8_0RzjRRZd7zvhaG9tpxw",
+  authDomain: "pellet-74b44.firebaseapp.com",
+  databaseURL: "https://pellet-74b44-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "pellet-74b44",
+  storageBucket: "pellet-74b44.appspot.com",
+  messagingSenderId: "83534619956",
+  appId: "1:83534619956:web:2c63c543b998824dfb3161",
+  measurementId: "G-WSNLY378Q7"
+};
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
